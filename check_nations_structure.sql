5f8ba21c58f3f5cf4efc5b2c95be5b297310b31c
SwineFeather <ajjaone123@gmail.com>
2025-07-29 11:56:28 +0200

    remove files

--- check_nations_structure.sql
+++
@@ -1,9 +0,0 @@
--- Check the actual structure of the nations table
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns 
-WHERE table_name = 'nations' 
-AND table_schema = 'public'
-ORDER BY ordinal_position;
-
--- Show a sample of nations data
-SELECT * FROM public.nations LIMIT 3; 
